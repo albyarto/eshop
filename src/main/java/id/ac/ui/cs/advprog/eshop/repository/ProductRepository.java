0d20b09f73af858c053fe52af63ce59b25956d31
albyarto <albyarto.07@gmail.com>
2025-02-12 22:35:54 +0700

    Create Edit Feature

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepository.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Repository;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public class ProductRepository {
@@ -18,5 +19,20 @@ public class ProductRepository {
 
     public Iterator<Product> findAll() {
         return productData.iterator();
+    }
+
+    public Optional<Product> findById(String id) {
+        return productData.stream()
+                .filter(product -> product.getProductId().equals(id))
+                .findFirst();
+    }
+
+    public void save(Product product) {
+        for (int i = 0; i < productData.size(); i++) {
+            if (productData.get(i).getProductId().equals(product.getProductId())) {
+                productData.set(i, product);
+                return;
+            }
+        }
     }
 }
