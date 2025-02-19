4b30c8df80f2371325bfe3db09f915b77f0d6a95
albyarto <albyarto.07@gmail.com>
2025-02-20 04:33:36 +0700

    fix: fix code quality issue (remove unnecessary modifier 'public' on some method in an interface type)

--- src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductService.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductService.java
@@ -4,9 +4,9 @@ import id.ac.ui.cs.advprog.eshop.model.Product;
 import java.util.List;
 
 public interface ProductService {
-    public Product create(Product product);
-    public List<Product> findAll();
-    public Product findById(String id);
-    public void update(String id, String productName, int productQuantity);
-    public void delete(String id);
+    Product create(Product product);
+    List<Product> findAll();
+    Product findById(String id);
+    void update(String id, String productName, int productQuantity);
+    void delete(String id);
 }
